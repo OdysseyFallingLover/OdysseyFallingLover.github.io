e9a6d5c6065101c5a7c23b5d069c89650783798b
OdysseyFallingLover <164561791+OdysseyFallingLover@users.noreply.github.com>
2024-03-29 19:40:56 +0300

    upd | v - 0.2

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 //import fetch
+
+const utils = {
+    rn: (int, fixed) => {
+        if (int === null) return null;
+        if (int === 0) return '0';
+        fixed = (!fixed || fixed < 0) ? 0 : fixed;
+        let b = (int).toPrecision(2).split('e'),
+            k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3),
+            c = k < 1 ? int.toFixed(0 + fixed) : (int / Math.pow(10, k * 3) ).toFixed(1 + fixed),
+            d = c < 0 ? c : Math.abs(c),
+            e = d + ['', 'K', 'M', 'B'][k];
+
+            e = e.replace(/e/g, '');
+            e = e.replace(/\+/g, '');
+
+        return e;
+    }
+}
+
+
 fetch('https://api.dexscreener.com/latest/dex/tokens/0x764BFC309090E7f93EDcE53E5BeFa374CDCB7b8e')
 .then(response => response.json())
 .then(data => {
-    document.getElementById('H2xPrice').innerText = "График цены Grimace/USDT: " + data.pairs[0].priceUsd;
+    document.getElementById('H1xPrice').innerText = "$" + data.pairs[0].priceUsd;
+    document.getElementById('H1xLiqd').innerText = "$" + utils.rn(data.pairs[0].liquidity.usd);
+    document.getElementById('H1xFDV').innerText = "$" + utils.rn(data.pairs[0].fdv);
+    document.getElementById('H1xMcup').innerText = "$" + utils.rn(data.pairs[0].fdv);
 });
