8ee8568e2925c455cdc4dc7e3fed9aa880097a08
OdysseyFallingLover <164561791+OdysseyFallingLover@users.noreply.github.com>
2024-04-07 04:45:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let tg = window.Telegram;
 if(tg != undefined) {
     if (tg.WebApp != undefined && tg.WebApp.initData != undefined) { 
         let safe = tg.WebApp.initData;
-        document.getElementById('H1xPrice').innerText = "s:} " + safe;
+        document.getElementById('H1xInfoCheck').innerText = "s:} " + safe;
         //tg.WebApp.backgroundColor = '#3d3d3d';
         //tg.WebApp.headerColor = '#212121';
         tg.WebApp.expand(); 
